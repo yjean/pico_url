d9ba762f0a913840c9b6506cd5b0ec02dce062d5
yjean <yoann.jean@gmail.com>
2009-01-07 15:24:57 +0100

    Avec cette version, on a un redirect automatique vers la Location dont on donne l'id en paramètre :
    http://localhost:3000/2 => redirect_to l'url de l'objet Location dont l'id est 2
    
    Pour l'instant, je ne sais pas faire de reprise sur erreur, etc ... mais déjà, c'est bien je trouve ^_^

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,5 +1,7 @@
 class RedirectController < ApplicationController
   def do
     @tinyurl = params[:tinyurl]
+    location = Location.find(params[:tinyurl])
+    redirect_to location.url, :status => :moved_permanently
   end
 end
