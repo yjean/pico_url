a1e11700df2a7b0542858522c02f576579066e7a
yjean <yoann.jean@gmail.com>
2009-01-08 09:59:55 +0100

    Methode encodage correct pour l id de location

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,12 @@ require "base64"
 
 class Location < ActiveRecord::Base
   validates_uniqueness_of :url # we need it so to store each url only once  
+  
+  def encoded
+    id.to_s(36)
+  end
+  
+  def decoded
+    id.to_s(36).to_i(36)
+  end
 end
